17cde00b615af00add82887ac3246e03af80aec6
Adharsh-Rengarajan <adharshr0613@gmail.com>
2025-06-08 23:01:36 +0530

    Account Interface Updated

---
+++ finalticapro-backend/src/main/java/com/finalticapro/application/entities/account/Account.java
@@ -0,0 +1,9 @@
+package com.finalticapro.application.entities.account;
+
+public interface Account {
+
+	public long getBalance();
+	public long updateBalance();
+	public String getCurrency();
+	
+}
